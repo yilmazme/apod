763fec6a1f7d3aab288bc1347dd6d2b0d180d87b
yilmazme <10myilmaz@gmail.com>
2020-09-12 15:02:51 +0300

    react-player added

---
+++ src/Apod.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+
+import ReactPlayer from "react-player";
+function Apod() {
+  const [image, setImage] = useState("");
+  const [hd, setHd] = useState(false);
+  const [error, setError] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=E4afWhPlSTYPAwdSxlEW3E7f23eyFZAue8sxN5FN"
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        setImage(data);
+        setLoaded(true);
+        console.log(data);
+      })
+      .catch((error) => {
+        setLoaded(true);
+        setError(error);
+      });
+  }, []); //empty for mimic cdm
+  const Display =
+    image.data_type === image ? (
+      <img src={hd ? image.hdurl : image.url} alt="photoofday" />
+    ) : (
+      <ReactPlayer width="100vw" height="80vw" controls url={image.url} />
+    );
+
+  if (error) {
+    return (
+      <div>There is a problem with source or your internet connection.</div>
+    );
+  } else if (!loaded) {
+    return <h3>Loading...</h3>;
+  } else {
+    return (
+      <div className="apod">
+        <h3>{image.date}</h3>
+        <h3>{image.title}</h3>
+        <button
+          onClick={() => {
+            setHd(!hd);
+          }}
+        >
+          {hd ? "Normal" : "For HD"}
+        </button>
+        {Display}
+        <p>
+          <b>About: </b>
+          {image.explanation}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Apod;
